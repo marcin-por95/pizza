0f79927b3c4b76e87d8e9264a5ce585dc60a7ca2
marcin-por95 <126095028+marcin-por95@users.noreply.github.com>
2023-04-21 19:04:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('dist/db/app.json');
+const router = jsonServer.router(path.join('dist', 'db', 'app.json'));
 const middlewares = jsonServer.defaults({
   static: 'dist',
   noCors: true
